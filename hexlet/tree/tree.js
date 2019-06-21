3b70bbd37bbea9cb111ddb420c2d2f1eb14223a7
Vadim Iskuchekov <root@egregors.com>
2019-06-21 21:27:26 +0200

    tree

---
+++ hexlet/tree/tree.js
@@ -0,0 +1,61 @@
+class Tree {
+    constructor(key, meta, parent) {
+        this.parent = parent;
+        this.key = key;
+        this.meta = meta;
+        this.children = new Map();
+    }
+
+    getKey() {
+        return this.key;
+    }
+
+    getMeta() {
+        return this.meta;
+    }
+
+    addChild(key, meta) {
+        const child = new Tree(key, meta, this);
+        this.children.set(key, child);
+
+        return child;
+    }
+
+    getChild(key) {
+        return this.children.get(key);
+    }
+
+    // BEGIN (write your solution here)
+    hasChildren() {
+        return !!this.children.size;
+    }
+
+    hasChild(key) {
+        return this.children.has(key);
+    }
+
+    getParent() {
+        return this.parent;
+    }
+
+    getChildren() {
+        return [...this.children.values()];
+    }
+
+    getDeepChild(keys) {
+        if (!keys.length) return undefined;
+        const child = this.getChild(keys[0]);
+        return child !== undefined
+            ? keys.length === 1
+                ? child
+                : child.getDeepChild(keys.slice(1))
+            : undefined;
+    }
+
+    removeChild(key) {
+        return this.children.delete(key);
+    }
+    // END
+}
+
+export default Tree;
