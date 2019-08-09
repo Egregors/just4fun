178bba4878e65c03685702eb439e785fe8f9fa97
Vadim Iskuchekov <root@egregors.com>
2019-08-09 19:30:42 +0200

    staff

--- hexlet/diff/__tests__/diff.test.js
+++ hexlet/diff/__tests__/diff.test.js
@@ -1,87 +1,111 @@
 import getDiff from "../diff";
 
-test("getDiff", () => {
-    const before = {
-        common: {
-            setting1: "Value 1",
-            setting2: "200",
-            setting3: true,
-            setting6: {
-                key: "value"
-            }
-        },
-        group1: {
-            baz: "bas",
-            foo: "bar",
-            nest: {
-                key: "value"
-            }
-        },
-        group2: {
-            abc: "12345"
-        }
+test("getDiff for one node", () => {
+    const b = {
+        host: "hexlet.io",
+        timeout: 50,
+        proxy: "123.234.53.22",
+        follow: false
     };
-    const after = {
-        common: {
-            follow: false,
-            setting1: "Value 1",
-            setting3: {
-                key: "value"
-            },
-            setting4: "blah blah",
-            setting5: {
-                key5: "value5"
-            },
-            setting6: {
-                key: "value",
-                ops: "vops"
-            }
-        },
-
-        group1: {
-            foo: "bar",
-            baz: "bars",
-            nest: "str"
-        },
-
-        group3: {
-            fee: "100500"
-        }
+    const a = {
+        timeout: 20,
+        verbose: true,
+        host: "hexlet.io"
     };
-    const diff = {
-        common: {
-            "+ follow": false,
-            setting1: "Value 1",
-            "- setting2": 200,
-            "- setting3": true,
-            "+ setting3": {
-                key: value
-            },
-            setting6: {
-                key: value,
-                "+ ops": vops
-            },
-            "+ setting4": "blah blah",
-            "+ setting5": {
-                key5: value5
-            }
-        },
-        group1: {
-            "+ baz": "bars",
-            "- baz": "bas",
-            foo: "bar",
-            "- nest": {
-                key: value
-            },
-            "+ nest": "str"
-        },
-        "- group2": {
-            abc: 12345
-        },
-        "+ group3": {
-            fee: 100500
-        }
+    const res = {
+        host: "hexlet.io",
+        "+ timeout": 20,
+        "- timeout": 50,
+        "- proxy": "123.234.53.22",
+        "+ verbose": true,
+        "- follow": false
     };
 
-    expect(getDiff(before, after)).toEqual(diff);
+    expect(getDiff(b, a)).toEqual(res);
 });
+
+// test("getDiff for file", () => {
+//     const before = {
+//         common: {
+//             setting1: "Value 1",
+//             setting2: "200",
+//             setting3: true,
+//             setting6: {
+//                 key: "value"
+//             }
+//         },
+//         group1: {
+//             baz: "bas",
+//             foo: "bar",
+//             nest: {
+//                 key: "value"
+//             }
+//         },
+//         group2: {
+//             abc: "12345"
+//         }
+//     };
+//     const after = {
+//         common: {
+//             follow: false,
+//             setting1: "Value 1",
+//             setting3: {
+//                 key: "value"
+//             },
+//             setting4: "blah blah",
+//             setting5: {
+//                 key5: "value5"
+//             },
+//             setting6: {
+//                 key: "value",
+//                 ops: "vops"
+//             }
+//         },
+
+//         group1: {
+//             foo: "bar",
+//             baz: "bars",
+//             nest: "str"
+//         },
+
+//         group3: {
+//             fee: "100500"
+//         }
+//     };
+//     const diff = {
+//         common: {
+//             "+ follow": false,
+//             setting1: "Value 1",
+//             "- setting2": 200,
+//             "- setting3": true,
+//             "+ setting3": {
+//                 key: value
+//             },
+//             setting6: {
+//                 key: value,
+//                 "+ ops": vops
+//             },
+//             "+ setting4": "blah blah",
+//             "+ setting5": {
+//                 key5: value5
+//             }
+//         },
+//         group1: {
+//             "+ baz": "bars",
+//             "- baz": "bas",
+//             foo: "bar",
+//             "- nest": {
+//                 key: value
+//             },
+//             "+ nest": "str"
+//         },
+//         "- group2": {
+//             abc: 12345
+//         },
+//         "+ group3": {
+//             fee: 100500
+//         }
+//     };
+
+//     expect(getDiff(before, after)).toEqual(diff);
+// });
--- hexlet/diff/diff.js
+++ hexlet/diff/diff.js
@@ -1,5 +1,64 @@
-const getDiff = () => {
-    
+import _ from "lodash";
+
+// todo:
+//  - [ ] stringify - to convert node to JSON | string
+//
+// cases:
+//  k: v -> k: v   =    k: v
+//  k: v -> k: v2  =  - k: v
+//                    + k: v2
+//  k: v -> -      =  - k: v
+//  -    -> k: v   =  + k: v
+//
+
+// helpers
+const getKeys = (a, b) => new Set(Object.keys(a).concat(Object.keys(b)));
+
+export const getDiff = (before, after) => {
+    const keys = getKeys(before, after);
+
+    const f = (acc, el) => {
+        // console.log(`${acc} : ${el}`);
+        // todo: dispatch this
+
+        if (!_.has(after, el)) {
+            //  k: v -> -      =  - k: v
+            return { ...acc, [`- ${el}`]: before[el] };
+        }
+
+        if (!_.has(before, el)) {
+            // -    -> k: v   =  + k: v
+            return { ...acc, [`+ ${el}`]: after[el] };
+        }
+
+        if (_.has(before, el) && _.has(after, el)) {
+            //  k: v -> k: v   =    k: v
+            //  k: v -> k: v2  =  - k: v
+            //                    + k: v2
+            return before[el] === after[el]
+                ? { ...acc, [el]: before[el] }
+                : { ...acc, [`- ${el}`]: before[el], [`+ ${el}`]: after[el] };
+        }
+
+        return acc;
+    };
+
+    return [...keys].reduce(f, {});
 };
 
 export default getDiff;
+
+const b = {
+    host: "hexlet.io",
+    timeout: 50,
+    proxy: "123.234.53.22",
+    follow: false
+};
+const a = {
+    timeout: 20,
+    verbose: true,
+    host: "hexlet.io"
+};
+
+const r = getDiff(b, a);
+console.log(r);
